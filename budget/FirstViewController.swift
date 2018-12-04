22306409522d6173513405a470ad49db8dbfd618
Annie Persson <annie.mc.persson@hotmail.com>
2018-12-04 11:14:08 +0000

    initial commit

---
+++ budget/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  budget
+//
+//  Created by Annie Persson on 04/12/2018.
+//  Copyright © 2018 Annie Persson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ budget/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  budget
+//
+//  Created by Annie Persson on 04/12/2018.
+//  Copyright © 2018 Annie Persson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
